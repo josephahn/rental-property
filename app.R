1ae99c63ccdb1a0b78df8364329282e94468af8f
Joseph Ahn <joseph.t.ahn@gmail.com>
2020-05-26 10:45:13 -0700

    add map

--- app.R
+++ app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(shinythemes)
 library(dplyr)
 library(tidyr)
+library(leaflet)
 
 # import data
 sales <- read.csv("data/sales.csv", check.names = FALSE)
@@ -20,14 +21,20 @@ rents_clean <- rents %>%
 
 ui <- fluidPage(
   navbarPage("Rental Property", theme = shinytheme("slate"),
-    tabPanel("Map", icon = icon("globe-americas")),
+    tabPanel("Map", icon = icon("globe-americas"),
+      leafletOutput("mymap")
+    ),
     tabPanel("Data", icon = icon("table")),
     tabPanel("About", icon = icon("info-circle"))
   )
 )
 
 server <- function(input, output, session) {
-
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      setView(lat=39.8283, lng=-98.5795, zoom=2)
+  })
 }
 
 shinyApp(ui = ui, server = server)
