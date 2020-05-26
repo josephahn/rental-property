6c750de08c2081171c44ddcb296a479094261ccd
Joseph Ahn <joseph.t.ahn@gmail.com>
2020-05-25 19:10:10 -0700

    import and clean data

--- app.R
+++ app.R
@@ -1,5 +1,22 @@
 library(shiny)
 library(shinythemes)
+library(dplyr)
+library(tidyr)
+
+# import data
+sales <- read.csv("data/sales.csv", check.names = FALSE)
+rents <- read.csv("data/rents.csv", check.names = FALSE)
+
+# clean data
+sales_clean <- sales %>%
+  filter(RegionName != "United States") %>%
+  separate(RegionName, c("City", "State"), sep = ", ") %>%
+  select(City, State, "2014-01":"2020-03")
+
+rents_clean <- rents %>%
+  filter(RegionName != "United States") %>%
+  separate(RegionName, c("City", "State"), sep = ", ") %>%
+  select(City, State, "2014-01":"2020-03")
 
 ui <- fluidPage(
   navbarPage("Rental Property", theme = shinytheme("slate"),
