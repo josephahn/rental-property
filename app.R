6e7ba01a269b7117fcf34f09a1ee6650d3fb5e48
Joseph Ahn <joseph.t.ahn@gmail.com>
2020-05-26 15:30:18 -0700

    add date slider

--- app.R
+++ app.R
@@ -3,6 +3,7 @@ library(shinythemes)
 library(dplyr)
 library(tidyr)
 library(leaflet)
+library(zoo)
 
 # import data
 sales <- read.csv("data/sales.csv", check.names = FALSE)
@@ -19,15 +20,35 @@ rents_clean <- rents %>%
   separate(RegionName, c("City", "State"), sep = ", ") %>%
   select(City, State, "2014-01":"2020-03")
 
+# variables
+first_date <- as.Date(as.yearmon("2014-01", "%Y-%m"))
+last_date <- as.Date(as.yearmon("2020-03", "%Y-%m"))
+
+# shiny ui
+
 ui <- fluidPage(
-  navbarPage("Rental Property", theme = shinytheme("slate"),
+  includeCSS("styles.css"),
+  navbarPage("Rental Property", theme = shinytheme("slate"), collapsible = TRUE,
     tabPanel("Map", icon = icon("globe-americas"),
-      leafletOutput("mymap")
+      div(class = "map-container",
+        leafletOutput("mymap", width = "100%", height = "100%"),
+        absolutePanel(top = 62, left = 54, width = 250, fixed=TRUE, draggable = TRUE, height = "auto",
+          sliderInput("date",
+            label = NULL,
+            min = first_date,
+            max = last_date,
+            value = last_date,
+            timeFormat="%b %Y",
+            animate = animationOptions(interval = 30, loop = FALSE))
+        )
+      )
     ),
     tabPanel("Data", icon = icon("table")),
     tabPanel("About", icon = icon("info-circle"))
   )
 )
+
+# shiny server
 
 server <- function(input, output, session) {
   output$mymap <- renderLeaflet({
